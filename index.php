79b11674613696b189a9b8b4a6bb6c6c95e9c23a
stephane teyssier <stephane.teyssier@wescale.fr>
2017-11-06 19:59:23 +0100

    Add basic PHP page

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+$instance_id = @file_get_contents("http://169.254.169.254/latest/meta-data/instance-id");
+?>
+<h1>It works!!</h1>
+<h2>Content served from EC2 by instance <?php echo $instance_id; ?>.</h2>
+</body>
+</html>
